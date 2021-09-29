f9a941a3055b4c471756251e45450e1487ac962d
Jose Miguel Cabrera <cabreraj525@gmal.com>
2021-09-29 00:10:18 -0600

    corregimos problema con base de datos

--- src/database.js
+++ src/database.js
@@ -2,7 +2,18 @@ import mysql from "mysql2/promise";
 import { config } from "./config.js";
 
 export const connect = async () => {
-  return await mysql.createConnection(config);
+  //console.log("creamos conexion");
+   const conexion=await mysql.createConnection(config);
+   await setTimeout(()=>{
+     try {
+        conexion.end()
+       //console.log("cerramos base de datos");
+     } catch (error) {
+       console.log(error);
+     }
+   },1500)
+  return conexion
 };
+
 
 //connect();
