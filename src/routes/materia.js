7aaf761860343a350a04defa485e28d9aa8555d6
Jose Miguel Cabrera <cabreraj525@gmal.com>
2021-10-13 21:31:25 -0600

    corregimos errores en consultas

--- src/routes/materia.js
+++ src/routes/materia.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { nuevaMateria,verMateria,eliminarMateria,editarMateria,verMateriaPorId } from "../controllers/materias";
+import { nuevaMateria,verMateria,eliminarMateria,editarMateria,verMateriaPorId ,verMateriaPorGrados} from "../controllers/materias";
 
 const router = Router()
 
@@ -8,6 +8,7 @@ router.get('/materia',verMateria)
 router.get('/materia/:id',verMateriaPorId)
 router.delete('/materia/:id',eliminarMateria)
 router.put('/materia/:id',editarMateria)
+router.get('/materia/grado/:id',verMateriaPorGrados)
 
 
 
